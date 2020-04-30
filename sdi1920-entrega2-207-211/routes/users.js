5d7c3d18909a88f04def6cc1ab8cd1e5e480e653
AGM <alejan1579@gmail.com>
2020-04-30 13:35:40 +0200

    User Signup

--- sdi1920-entrega2-207-211/routes/users.js
+++ sdi1920-entrega2-207-211/routes/users.js
@@ -7,3 +7,25 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+module.exports = function (app, swig, gestorBD) {
+  app.get("/signup", function (req, res) {
+    let response = swig.renderFile('views/bsignup.html',{});
+    res.send(response);
+  });
+  app.post('/user', function (req, res) {
+    let hash = app.get("crypto").createHmac('sha256', app.get('key'))
+        .update(req.body.password).digest('hex');
+    let user = {
+      email: req.body.email,
+      password: hash
+    };
+    gestorBD.insertUser(user, function (id) {
+      if (id == null) {
+        res.redirect("/signup?message=Error al registrar usuario")
+      } else {
+        res.redirect("/signup?message=Nuevo usuario registrado");
+      }
+    });
+  })
+};
