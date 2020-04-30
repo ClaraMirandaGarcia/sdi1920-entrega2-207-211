764d9f7629861a0d6ee61cfa6e6646acd1eac23e
AGM <alejan1579@gmail.com>
2020-04-30 12:59:31 +0200

    Refactor app.js

--- sdi1920-entrega2-207-211/app.js
+++ sdi1920-entrega2-207-211/app.js
@@ -1,22 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let express = require('express');
+let app = express();
+
+let crypto = require('crypto');
+let path = require('path');
+let fs = require('fs');
+let https = require('https');
+let mongo = require('mongodb');
+let swig = require('swig');
+let bodyParser = require('body-parser');
+// Variables
+app.set('port', 8081);
+app.set('db', 'mongodb://admin:207sdi@tiendamusica-shard-00-00-lpbsd.mongodb.net:27017,tiendamusica-shard-00-01-lpbsd.mongodb.net:27017,tiendamusica-shard-00-02-lpbsd.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true&w=majority');
+app.set('key', 'abcdefg');
+app.set('crypto', crypto);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.set('view engine', 'pug') ;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -38,4 +44,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+//lanzar el servidor
+https.createServer({
+  key: fs.readFileSync('certificates/alice.key'),
+  cert: fs.readFileSync('certificates/alice.crt')
+}, app).listen(app.get('port'), function () {
+  console.log("Servidor activo");
+});
