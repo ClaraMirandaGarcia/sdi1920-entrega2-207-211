71b21adbd905a23a8de3ef440e3067331658a1ba
Clara <UO264958@uniovi.es>
2020-05-11 21:48:52 +0200

    Added database management class

--- sdi1920-entrega2-test-207-211/src/com/uniovi/tests/util/DatabaseUtils.java
+++ sdi1920-entrega2-test-207-211/src/com/uniovi/tests/util/DatabaseUtils.java
@@ -1,111 +1,110 @@
 package com.uniovi.tests.util;
 
-
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 
 public class DatabaseUtils {
 
-	
-	/**
-	 * Aborta si el "texto" no está presente en la página actual
-	 * @param driver: apuntando al navegador abierto actualmente.
-	 * @param texto: texto a buscar
-	 */
-	static public void textoPresentePagina(WebDriver driver, String texto)
-	{
-		List<WebElement> list = driver.findElements(By.xpath("//*[contains(text(),'" + texto + "')]"));		
-		assertTrue("Texto " + texto + " no localizado!", list.size() > 0);			
+	static MongoClient mongoClient;
+	private final static String mongoUri = "mongodb://admin:207sdi@tiendamusica-shard-00-00-lpbsd.mongodb.net:27017,tiendamusica-shard-00-01-lpbsd.mongodb.net:27017,tiendamusica-shard-00-02-lpbsd.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true&w=majority";
+	private final static String nameDatabase = "test";
+
+	private static MongoDatabase getConnection() {
+		MongoClientURI uri = new MongoClientURI(mongoUri);
+		mongoClient = new MongoClient(uri);
+		MongoDatabase database = mongoClient.getDatabase(nameDatabase);
+		return database;
 	}
 
-	/**
-	 * Aborta si el "texto" está presente en la página actual
-	 * @param driver: apuntando al navegador abierto actualmente.
-	 * @param texto: texto a buscar
-	 */
-	static public void textoNoPresentePagina(WebDriver driver, String texto)
-	{
-		List<WebElement> list = driver.findElements(By.xpath("//*[contains(text(),'" + texto + "')]"));		
-		assertTrue("Texto " + texto + " aun presente !", list.size() == 0);			
+	public static void closeConnection() {
+		mongoClient.close();
 	}
 
-	/**
-	 * Aborta si el "texto" está presente en la página actual tras timeout segundos.
-	 * @param driver: apuntando al navegador abierto actualmente.
-	 * @param texto: texto a buscar
-	 * @param timeout: el tiempo máximo que se esperará por la aparición del texto a buscar
-	 */
-	static public void EsperaCargaPaginaNoTexto(WebDriver driver, String texto, int timeout)
-	{
-		Boolean resultado = 
-				(new WebDriverWait(driver, timeout)).until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[contains(text(),'" + texto + "')]")));
+	public static void removeAllMessagesUsers(String emailUserFrom, String emailUserTo) {
+		// obtener database
+		MongoDatabase db = getConnection();
 
-		assertTrue(resultado);	
+		// obtener coleccion
+		MongoCollection<Document> messagesCollection = db.getCollection("messages");
+		Bson filterDelete = Filters.or(
+				Filters.and(Filters.eq("origen", (emailUserFrom)), Filters.eq("destino", (emailUserTo))),
+				Filters.and(Filters.eq("origen", (emailUserTo)), Filters.eq("destino", (emailUserFrom))));
+		messagesCollection.deleteMany(filterDelete);
 	}
 
-
-	/**
-	 * Espera por la visibilidad de un elemento/s en la vista actualmente cargandose en driver. Para ello se empleará una consulta xpath.
-	 * @param driver: apuntando al navegador abierto actualmente.
-	 * @param xpath: consulta xpath.
-	 * @param timeout: el tiempo máximo que se esperará por la aparición del elemento a buscar con xpath
-	 * @return  Se retornará la lista de elementos resultantes de la búsqueda con xpath.
-	 */
-	static public List<WebElement> EsperaCargaPaginaxpath(WebDriver driver, String xpath, int timeout)
-	{
-		WebElement resultado = 
-				(new WebDriverWait(driver, timeout)).until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
-		assertTrue(resultado != null);
-		List<WebElement> elementos = driver.findElements(By.xpath(xpath));
-
-		return elementos;					
+	public static int countUsers() {
+		// obtener database
+		MongoDatabase db = getConnection();
+		// obtener coleccion
+		MongoCollection<Document> usersCollection = db.getCollection("users");
+		return (int) usersCollection.count();
 	}
 
-	/**
-	 * Espera por la visibilidad de un elemento/s en la vista actualmente cargandose en driver. Para ello se empleará una consulta xpath 
-	 * según varios criterios..
-	 * 
-	 * @param driver: apuntando al navegador abierto actualmente.
-	 * @param criterio: "id" or "class" or "text" or "@attribute" or "free". Si el valor de criterio es free es una expresion xpath completa. 
-	 * @param text: texto correspondiente al criterio.
-	 * @param timeout: el tiempo máximo que se esperará por la apareción del elemento a buscar con criterio/text.
-	 * @return Se retornará la lista de elementos resultantes de la búsqueda.
-	 */
-	static public List<WebElement> EsperaCargaPagina(WebDriver driver, String criterio, String text, int timeout)
-	{
-		String busqueda;
-		if (criterio.equals("id")) busqueda = "//*[contains(@id,'" + text + "')]";
-		else if (criterio.equals("class")) busqueda = "//*[contains(@class,'" + text + "')]";
-		else if (criterio.equals("text")) busqueda = "//*[contains(text(),'" + text + "')]";
-		else if (criterio.equals("free")) busqueda = text;
-		else busqueda = "//*[contains("+criterio+",'" + text + "')]";
+	public static String getIdFromUserEmail(String email) {
+		// obtener database
+		MongoDatabase db = getConnection();
 
-		return EsperaCargaPaginaxpath(driver, busqueda, timeout);
-	}
-
-
-	/**
-	 * PROHIBIDO USARLO PARA VERSIÓN FINAL.
-	 * Esperar "segundos" durante la ejecucion del navegador 
-	 * @param driver: apuntando al navegador abierto actualmente.
-	 * @param segundos: Segundos de bloqueo de la ejecución en el navegador.
-	 */
-	static public void esperarSegundos(WebDriver driver, int segundos){
-
-		synchronized(driver){
-			try {
-				driver.wait(segundos * 1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		// obtener coleccion
+		MongoCollection<Document> collection = db.getCollection("users");
+		Bson bsonFilter = Filters.eq("email", email);
+		FindIterable<Document> docs = collection.find(bsonFilter);
+		for (Document doc : docs) {
+			return doc.get("_id").toString();
 		}
+		return null;
 	}
+
+	public static void createFriendship(String emailUserFrom, String emailUserTo) {
+
+		// obtener ids
+		String idUserFrom = getIdFromUserEmail(emailUserFrom);
+		String idUserTo = getIdFromUserEmail(emailUserTo);
+
+		// obtener database
+		MongoDatabase db = getConnection();
+
+		// obtener coleccion
+		MongoCollection<Document> friendshipCollection = db.getCollection("friendship");
+		List<Document> docList = new ArrayList<>();
+
+		Document friendshipDoc = new Document();
+		friendshipDoc.append("userTo", new ObjectId(idUserTo));
+		friendshipDoc.append("userFrom", new ObjectId(idUserFrom));
+		docList.add(friendshipDoc);
+
+		friendshipCollection.insertMany(docList);
+	}
+
+	public static void removeFriendship(String emailUserFrom, String emailUserTo) {
+
+		// obtener ids
+		String idUserFrom = getIdFromUserEmail(emailUserFrom);
+		String idUserTo = getIdFromUserEmail(emailUserTo);
+
+		// obtener database
+		MongoDatabase db = getConnection();
+
+		// obtener coleccion
+		MongoCollection<Document> friendshipCollection = db.getCollection("friendship");
+
+		Bson friendshipFilter = Filters.or(
+				Filters.and(Filters.eq("userFrom", new ObjectId(idUserFrom)),
+						Filters.eq("userTo", new ObjectId(idUserTo))),
+				Filters.and(Filters.eq("userFrom", new ObjectId(idUserTo)),
+						Filters.eq("userTo", new ObjectId(idUserFrom))));
+
+		friendshipCollection.deleteMany(friendshipFilter);
+	}
+
 }
